f0b0da076fc51553b8daed3cac55dbf13ced3149
Sergio Medina <hola@sergiomedina.io>
2023-03-23 20:13:16 -0300

    chore: add clase_02 setup

---
+++ clase_02/index.js
@@ -0,0 +1,9 @@
+const imagenPerro = "https://img.freepik.com/foto-gratis/lindo-perrito-haciendose-pasar-persona-negocios_23-2148985938.jpg";
+const imagenGato = "https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg";
+
+const app = new Vue({
+    el: '#contenedor',
+    data: {
+        mostrar: true,
+    }
+});
