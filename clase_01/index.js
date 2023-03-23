513121c4f6930b33a1f8bc6f6d9967c8a1111226
Sergio Medina <hola@sergiomedina.io>
2023-03-23 20:10:54 -0300

    feat: add image selector with checkbox

--- clase_01/index.js
+++ clase_01/index.js
@@ -1,9 +1,14 @@
+const imagenPerro = "https://img.freepik.com/foto-gratis/lindo-perrito-haciendose-pasar-persona-negocios_23-2148985938.jpg";
+const imagenGato = "https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg";
+
 const app = new Vue({
     el: '#contenedor',
     data: {
+        elegido: false,
         mensaje: 'Hello Vue!',
         mostrar: true,
         anchoImagen: 200,
-        urlElegida: "https://img.freepik.com/foto-gratis/lindo-perrito-haciendose-pasar-persona-negocios_23-2148985938.jpg"
+        urlElegida: imagenPerro,
+        imagenGato,
     }
 });
