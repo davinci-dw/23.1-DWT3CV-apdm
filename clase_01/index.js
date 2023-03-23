92381fb227160517458a1c82b07b85bfec1195c1
Sergio Medina <hola@sergiomedina.io>
2023-03-23 19:26:41 -0300

    feat: add dog image with size

--- clase_01/index.js
+++ clase_01/index.js
@@ -2,6 +2,8 @@ const app = new Vue({
     el: '#contenedor',
     data: {
         mensaje: 'Hello Vue!',
-        mostrar: true
+        mostrar: true,
+        anchoImagen: 200,
+        urlElegida: "https://img.freepik.com/foto-gratis/lindo-perrito-haciendose-pasar-persona-negocios_23-2148985938.jpg"
     }
 });
