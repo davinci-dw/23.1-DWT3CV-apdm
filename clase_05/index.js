dace94492c73c2de61f9dc164ed79604d00d38d3
Sergio Medina <hola@sergiomedina.io>
2023-04-20 19:45:18 -0300

    chore: add clase_05

---
+++ clase_05/index.js
@@ -0,0 +1,49 @@
+const imagenPerro = "https://img.freepik.com/foto-gratis/lindo-perrito-haciendose-pasar-persona-negocios_23-2148985938.jpg";
+const imagenGato = "https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg";
+
+const app = new Vue({
+    el: '#contenedor',
+    data: {
+        panelLoginVisible: false,
+        mensaje: 'Vamos a armar la aplicación en Vue modularmente',
+        login: false,
+        links: [
+            {text: 'Información general', url: '/home', enable: true, active: true},
+            {text: 'Mi perfil', url: '/about', enable: false, active: false},
+            {text: 'Configuraciones', url: '/contact', enable: false, active: false},
+        ]
+    },
+    methods: {
+        mostrarPanelLogin() {
+            this.panelLoginVisible = true;
+        },
+        ocultarPanelLogin() {
+            this.panelLoginVisible = false;
+        },
+        iniciarSesion() {
+            this.login = true;
+            this.ocultarPanelLogin();
+        },
+        toogleLogin() {
+            if(!this.login) {
+                this.mostrarPanelLogin();
+            }
+            this.login = !this.login;
+        },
+        envioDeInformacion() {
+            console.log('Se ha enviado la información');
+        },
+        probandoEscape() {
+            console.log("probandoEscape");
+            alert('Se ha presionado la tecla escape');
+        },
+        agregarMiPerfil() {
+            const miPerfil = {url: '/about', enable: true};
+            this.links.push(miPerfil);
+        },
+        sacarMiPerfil() {
+            this.links.splice(1, 1);
+            console.log('Se ha sacado el perfil');
+        }
+    }
+});
