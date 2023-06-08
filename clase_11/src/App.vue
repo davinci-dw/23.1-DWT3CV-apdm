6a1e22cd9a3a85cf281231b4b3a43bd72a5fbfab
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-06-08 19:24:40 -0300

    chore: add clase_11 structure

---
+++ clase_11/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HeaderApp/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Estamos probando vue2 via cli"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HeaderApp from './components/HeaderApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderApp,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
