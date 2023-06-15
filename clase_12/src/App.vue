1f8d7a612b5f82f95f091930209bf120dad92331
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-06-15 20:39:12 -0300

    feat: add drawer with links

--- clase_12/src/App.vue
+++ clase_12/src/App.vue
@@ -1,6 +1,44 @@
 <template>
   <div id="app">
   <v-app>
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+          dense
+          nav
+      >
+        <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <router-link :to="item.link">
+                {{ item.title }}
+              </router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
       color="primary"
@@ -26,7 +64,13 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
+    items: [
+      { title: 'Información General', icon: 'mdi-view-dashboard', link: '/' },
+      { title: 'Mi perfil', icon: 'mdi-image', link: '/about' },
+      { title: 'Configuración', icon: 'mdi-help-box', link: '/contact' },
+    ],
+    right: null,
   }),
 };
 </script>
